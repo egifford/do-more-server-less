505b409774650320d05914d1807b3a6e1acde5c8
Richard Moot <moot@squareup.com>
2018-06-05 13:24:36 -0700

    Add additional slides and logos

---
+++ src/AzureLogo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AzureLogo = props => (
+  <svg height={props.size ? props.size : "90%"} viewBox="0 0 161.67 129" width={props.size ? props.size : "90%"} {...props}>
+    <path
+      d="M88.33 0L40.67 41.33 0 114.33h36.67zm6.34 9.67L74.33 67l39 49-75.66 13h124z"
+      fill="#0072c6"
+    />
+  </svg>
+);
+
+export default AzureLogo;
